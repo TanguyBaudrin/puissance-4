ffa2da5d0b889bf9edd1a88395ab0d7502e3f158
Tangeek59 <tanguy@baudrin.fr>
2021-10-01 15:11:54 +0200

    feat(main): Initialize main.py file

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the puissance 4 game")
+player1 = input("Choose player 1 name : ")
+player2 = input("Choose player 2 name : ")
+
+print(f"Well, {player1} it's yout turn, choose a colmun where you want to drop your token")
+column = input("Column # : ")
